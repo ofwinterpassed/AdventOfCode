58c5e4c5558e2a7d5b9d05f160b228c2b6ae0d78
Johannes Christenson <johannes.christenson@king.com>
2020-12-08 08:53:33 +0100

    formatting

--- 2020/day3/part2better.cpp
+++ 2020/day3/part2better.cpp
@@ -17,15 +17,14 @@ int main(int argc, char **argv) {
   }
 
   auto countTrees = [&](int hDiff, int vDiff) -> long {
-    return get<2>(accumulate(next(values.cbegin()), values.cend(), tuple{0, 1, 0},
-                             [=](auto acc, const auto &line) {
-                               auto &[hIndex, vIndex, sum] = acc;
-                               if (vIndex++ % vDiff == 0) {
-                                 hIndex += hDiff;
-                                 sum += line[hIndex % line.size()] == '#';
-                               }
-                               return acc;
-                             }));
+    return get<2>(accumulate(next(values.cbegin()), values.cend(), tuple{0, 1, 0}, [=](auto acc, const auto &line) {
+      auto &[hIndex, vIndex, sum] = acc;
+      if (vIndex++ % vDiff == 0) {
+        hIndex += hDiff;
+        sum += line[hIndex % line.size()] == '#';
+      }
+      return acc;
+    }));
   };
 
   long numTrees = countTrees(1, 1) * countTrees(3, 1) * countTrees(5, 1) * countTrees(7, 1) * countTrees(1, 2);
