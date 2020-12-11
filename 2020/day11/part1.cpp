97ca3217c4605f0ab73f109c162080415b8df07b
Johannes Christenson <johannes.christenson@king.com>
2020-12-11 08:03:19 +0100

    2020 day11

---
+++ 2020/day11/part1.cpp
@@ -0,0 +1,73 @@
+#include <algorithm>
+#include <array>
+#include <charconv>
+#include <fstream>
+#include <iostream>
+#include <numeric>
+#include <regex>
+#include <stack>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+long accumulator = 0;
+
+enum class instruction_t : char { acc, jmp, nop };
+
+int main(int argc, char **argv) {
+
+  vector<string> input;
+  input.emplace_back();
+  for (string line; getline(cin, line);) {
+    input.emplace_back(".").append(std::move(line)).append(".");
+  }
+  input.emplace_back();
+  input.front().resize(input[1].size(), '.');
+  input.back().resize(input[1].size(), '.');
+
+  auto countTaken = [&](auto row, auto column) {
+    int taken = 0;
+    taken += input[row - 1][column - 1] == '#';
+    taken += input[row - 1][column] == '#';
+    taken += input[row - 1][column + 1] == '#';
+    taken += input[row][column - 1] == '#';
+    taken += input[row][column + 1] == '#';
+    taken += input[row + 1][column - 1] == '#';
+    taken += input[row + 1][column] == '#';
+    taken += input[row + 1][column + 1] == '#';
+    return taken;
+  };
+
+  auto cp = input;
+  bool stable = false;
+  while (!stable) {
+    stable = true;
+    for (size_t row = 1; row < (input.size() - 1); ++row) {
+      for (size_t column = 1; column < (input[row].size() - 1); ++column) {
+        int taken = countTaken(row, column);
+        switch (input[row][column]) {
+        case 'L': {
+          if (taken == 0) {
+            cp[row][column] = '#';
+            stable = false;
+          }
+          break;
+        }
+        case '#': {
+          if (taken >= 4) {
+            cp[row][column] = 'L';
+            stable = false;
+          }
+          break;
+        }
+        default:;
+        }
+      }
+    }
+    input = cp;
+  }
+  cout << transform_reduce(cp.begin(), cp.end(), 0, plus<>(), [](auto &&line) {
+    return count(line.begin(), line.end(), '#');
+  }) << '\n';
+}
---
+++ 2020/day11/part2.cpp
@@ -0,0 +1,93 @@
+#include <algorithm>
+#include <array>
+#include <charconv>
+#include <fstream>
+#include <iostream>
+#include <numeric>
+#include <regex>
+#include <stack>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+long accumulator = 0;
+
+enum class instruction_t : char { acc, jmp, nop };
+
+int main(int argc, char **argv) {
+
+  vector<string> input;
+  input.emplace_back();
+  for (string line; getline(cin, line);) {
+    input.emplace_back(".").append(std::move(line)).append(".");
+  }
+  input.emplace_back();
+  input.front().resize(input[1].size(), '.');
+  input.back().resize(input[1].size(), '.');
+
+  const size_t bottom = 0u, top = input.size() - 1, left = 0u, right = input[0].size() - 1;
+
+  auto findInDir = [&](auto row, auto column, auto rd, auto cd) {
+    row += rd;
+    column += cd;
+    while (row > bottom && row < top && column > left && column < right) {
+      if (input[row][column] == '#')
+        return true;
+      else if (input[row][column] == 'L')
+        return false;
+      row += rd;
+      column += cd;
+    }
+    return false;
+  };
+  auto countTaken = [&](auto row, auto column) {
+    int taken = 0;
+    taken += findInDir(row, column, -1, -1);
+    taken += findInDir(row, column, 0, -1);
+    taken += findInDir(row, column, 1, -1);
+    taken += findInDir(row, column, -1, 0);
+    taken += findInDir(row, column, 1, 0);
+    taken += findInDir(row, column, -1, 1);
+    taken += findInDir(row, column, 0, 1);
+    taken += findInDir(row, column, 1, 1);
+    return taken;
+  };
+
+  auto cp = input;
+  bool stable = false;
+  while (!stable) {
+    stable = true;
+    for (size_t row = 1; row < (input.size() - 1); ++row) {
+      for (size_t column = 1; column < (input[row].size() - 1); ++column) {
+        int taken = countTaken(row, column);
+        switch (input[row][column]) {
+        case 'L': {
+          if (taken == 0) {
+            cp[row][column] = '#';
+            stable = false;
+          }
+          break;
+        }
+        case '#': {
+          if (taken >= 5) {
+            cp[row][column] = 'L';
+            stable = false;
+          }
+          break;
+        }
+        default:;
+        }
+      }
+    }
+    input = cp;
+    for (auto &&line : input)
+      clog << line << '\n';
+    clog << endl;
+  }
+  for (auto &&line : input)
+    clog << line << '\n';
+  cout << transform_reduce(cp.begin(), cp.end(), 0, plus<>(), [](auto &&line) {
+    return count(line.begin(), line.end(), '#');
+  }) << '\n';
+}
