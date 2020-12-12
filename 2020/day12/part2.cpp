547a698103817e8f600d04f2ac3a2133813b5be6
Johannes Christenson <johannes.christenson@king.com>
2020-12-12 07:07:49 +0100

    2020 day12

---
+++ 2020/day12/part2.cpp
@@ -0,0 +1,70 @@
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <charconv>
+#include <complex>
+#include <fstream>
+#include <iostream>
+#include <numeric>
+#include <regex>
+#include <stack>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+
+  complex<float> pos{0, 0};
+  complex<float> dir{10, 1};
+
+  for (string line; getline(cin, line);) {
+    int val;
+    from_chars(line.data() + 1, line.data() + line.size(), val);
+	float value = static_cast<float>(val);
+    switch (line[0]) {
+    case 'F': {
+      pos += value * dir;
+    } break;
+    case 'R': {
+      switch (val) {
+      default:
+        assert(false);
+        break;
+      case 90:
+        dir *= complex<float>{0, 1};
+      case 180:
+        dir *= complex<float>{0, 1};
+      case 270:
+        dir *= complex<float>{0, 1};
+      }
+    } break;
+    case 'L': {
+      switch (val) {
+      default:
+        assert(false);
+        break;
+      case 90:
+        dir *= complex<float>{0, -1};
+      case 180:
+        dir *= complex<float>{0, -1};
+      case 270:
+        dir *= complex<float>{0, -1};
+      }
+    } break;
+    case 'E': {
+      dir += value;
+    } break;
+    case 'W': {
+      dir -= value;
+    } break;
+    case 'S': {
+      dir -= complex<float>{0, value};
+    } break;
+    case 'N': {
+      dir += complex<float>{0, value};
+    } break;
+    }
+  }
+  cout << std::abs(pos.real()) + std::abs(pos.imag()) << '\n';
+}
