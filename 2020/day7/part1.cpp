0469f3d69a47880436227ddc6bc2b1f0d612662b
Johannes Christenson <johannes.christenson@king.com>
2020-12-18 17:04:16 +0100

    faster day7

--- 2020/day7/part1.cpp
+++ 2020/day7/part1.cpp
@@ -20,19 +20,19 @@ int main(int argc, char **argv) {
   stack<string> bagsToMatch;
   bagsToMatch.push("shiny gold");
   while (!bagsToMatch.empty()) {
-    regex r(string("(.*) bags contain .*") + bagsToMatch.top() + ".*");
+	string toMatch = move(bagsToMatch.top());
     bagsToMatch.pop();
     for (auto &rule : input) {
-      smatch match;
-      if (regex_match(rule, match, r) && find(matching.cbegin(), matching.cend(), match[1].str()) == matching.cend()) {
-        matching.emplace_back(match[1].str());
-        bagsToMatch.push(match[1].str());
+			auto del = rule.find(" bags contain ");
+      if ((rule.find(toMatch, del) != string::npos) && find(matching.cbegin(), matching.cend(), rule.substr(0, del)) == matching.cend()) {
+        matching.emplace_back(rule.substr(0, del));
+        bagsToMatch.emplace(rule.substr(0, del));
       }
     }
   }
-  for (auto bag : matching) {
-    clog << bag << "\n";
-  }
+  //for (auto bag : matching) {
+    //clog << bag << "\n";
+  //}
 
   cout << matching.size() << '\n';
 }
