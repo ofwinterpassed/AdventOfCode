be11627264c247ed292479f8e2b50fc692a971cf
Johannes Christenson <johannes.christenson@king.com>
2020-12-24 10:30:17 +0100

    day23 part2 optimized even further

--- 2020/day23/part2.cpp
+++ 2020/day23/part2.cpp
@@ -1,73 +1,39 @@
 #include <algorithm>
 #include <charconv>
 #include <iostream>
-#include <iterator>
-#include <list>
+#include <numeric>
 #include <string>
 #include <vector>
 
 using namespace std;
 
-struct node {
-  int prev, next;
-};
-template <int N> struct mylist {
-  using value_type = node;
-  mylist() : store{N} {
-    store[0].prev = store[0].next = 0;
-  }
-  void insert(int c) {
-    auto& n = store[c] = {store[0].prev, 0};
-    store[n.prev].next = c;
-    store[n.next].prev = c;
-  }
-  vector<node> store;
-};
-
 int main(int argc, char** argv) {
 
   string line;
   getline(cin, line);
-  mylist<1000000> numbers;
-  for (int a = 0; a < line.size(); ++a) {
-    int i = line[a] - '0' - 1;
-    numbers.insert(i);
-  }
+  vector<int> numbers(1000000);
+  int prev = 999999;
+  for (char a : line)
+    prev = numbers[prev] = a - '0' - 1;
+  numbers[prev] = 9;
 
-  for (int i = 9; i < 1000000; ++i) {
-    numbers.store[i] = {i - 1, i + 1};
-  }
-  numbers.store[0].prev = 999999;
-  numbers.store[5].next = 9;
-  numbers.store[9].prev = 5;
-  numbers.store[999999].next = 0;
+  iota(numbers.begin() + 9, numbers.end() - 1, 10);
 
   int currentval = 0;
   for (int i = 0; i < 10000000; ++i) {
-	int next1val = numbers.store[currentval].next;
-    node* next1 = &numbers.store[next1val];
-	int next2val = next1->next;
-    node* next2 = &numbers.store[next2val];
-	int next3val = next2->next;
-    node* next3 = &numbers.store[next3val];
-    int destination = currentval - 1;
-    if (destination == -1)
-      destination = 999999;
-    while ((destination == next1val) || (destination == next2val) || (destination == next3val)) {
-      destination = destination - 1;
-      if (destination == -1)
-        destination = 999999;
-    }
-    numbers.store[next1->prev].next = next3->next;
-    numbers.store[next3->next].prev = exchange(next1->prev, destination);
-    int destafter = exchange(numbers.store[destination].next, next1val);
-    next3->next = destafter;
-    numbers.store[destafter].prev = next3val;
-    currentval = numbers.store[currentval].next;
+    int next1val = numbers[currentval];
+    int next2val = numbers[next1val];
+    int next3val = numbers[next2val];
+    int destination = currentval;
+    do {
+      destination = destination == 0 ? 999999 : destination - 1;
+    } while ((destination == next1val) || (destination == next2val) || (destination == next3val));
+    numbers[currentval] = exchange(numbers[next3val], numbers[destination]);
+    numbers[destination] = next1val;
+    currentval = numbers[currentval];
   }
-  node* oneIter = &numbers.store[0];
-  int next1 = oneIter->next;
-  int next2 = numbers.store[oneIter->next].next;
+  int next1 = numbers[0];
+  int next2 = numbers[next1];
 
   cout << long(next1 + 1) * long(next2 + 1) << '\n';
 
